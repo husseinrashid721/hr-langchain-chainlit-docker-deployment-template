6f3e8ea9624526340517420306a43f2eb35be990
Hussein Rashid <58913314+husseinrashid721@users.noreply.github.com>
2025-04-26 10:21:53 -0400

    Update main.py

--- demo_app/main.py
+++ demo_app/main.py
@@ -16,11 +16,23 @@ template = """Question: {question}
 
 Answer: Let's think step by step."""
 
-@cl.langchain_factory(use_async=True)
-def factory():
-    user_env = cl.user_session.get("env")
-    os.environ["OPENAI_API_KEY"] = user_env.get("OPENAI_API_KEY")
-    prompt = PromptTemplate(template=template, input_variables=["question"])
-    llm_chain = LLMChain(prompt=prompt, llm=OpenAI(temperature=0), verbose=True)
+# @cl.langchain_factory(use_async=True)
+# def factory():
+#     user_env = cl.user_session.get("env")
+#     os.environ["OPENAI_API_KEY"] = user_env.get("OPENAI_API_KEY")
+#     prompt = PromptTemplate(template=template, input_variables=["question"])
+#     llm_chain = LLMChain(prompt=prompt, llm=OpenAI(temperature=0), verbose=True)
 
-    return llm_chain
+#     return llm_chain
+
+import chainlit as cl
+
+
+@cl.on_message
+async def main(message: cl.Message):
+    # Your custom logic goes here...
+
+    # Send a response back to the user
+    await cl.Message(
+        content=f"Received: {message.content}",
+    ).send()
